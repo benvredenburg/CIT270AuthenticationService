a096a1aec8ed421732023e972960c6aaba79d206
benvredenburg <vre19001@byui.edu>
2022-05-04 13:51:08 -0600

    05/04/2022

--- apiclient.bat
+++ apiclient.bat
@@ -1,1 +1,1 @@
-curl -d "@data.json" -X POST https://dev.stedi.me/login
+curl -d "@data.json" -X POST http://localhost:3000/login
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express'); // import library
+const bodyParser = require('body-parser'); // import body parser
 const port = 3000;
 const app = express(); // use library
+
+app.use(bodyParser.json()); 
 
 app.listen(port, ()=>{
     console.log('listening on port: ' + port);
 }) // listen
 
+app.post('/login',(request, response) =>{
+    const loginRequest = request.body;
+    if (loginRequest.userName == "vredenburgben@gmail.com" && loginRequest.password =="N7T5PpQz!") {
+        response.status(200);
+        response.send('Welcome');
+    } else {
+        response.status(401);
+        response.send('Unauthorized');
+    }
+});
+
 app.get('/',(request,response)=>{
     response.send('Hello, welcome to localhost')
 }); // response
+
