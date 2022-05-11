71750819e8f7d549370f56e98d9620dce5756180
benvredenburg <vre19001@byui.edu>
2022-05-11 13:47:00 -0600

    05/11/2022

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express'); // import library
 const bodyParser = require('body-parser'); // import body parser
 const port = 3000;
 const app = express(); // use library
+const md5 = required('md5');
 
 app.use(bodyParser.json()); // Use body parser
 
@@ -10,7 +11,9 @@ app.listen(port, ()=>{
 }) // listen
 
 app.post('/login',(request, response) =>{
+
     const loginRequest = request.body;
+    console.log("Request Body", JSON.stringify(request.body));
     if (loginRequest.userName == "vredenburgben@gmail.com" && loginRequest.password =="N7T5PpQz!") {
         response.status(200);
         response.send('Welcome');
