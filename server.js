8c3469ec6c33b025ba74d4f29f9ee7887b30291d
benvredenburg <vre19001@byui.edu>
2022-05-13 13:49:51 -0600

    05/13/2022

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const express = require('express'); // import library
+const express = require('express'); // import express library
 const bodyParser = require('body-parser'); // import body parser
-const port = 3000;
-const app = express(); // use library
-const md5 = required('md5');
+const port = 3000; // define port const variable
+const app = express(); // use express library
+const md5 = require('md5'); // import md5
+const redis = require('redis');
+
+const redisClient = redis.createClient();
+
+/*
+x Create a redis hashmap in the redis-cli
+x hash your password md5.online.com
+x install the node-redis library (npm install node-redis)
+x require the node-redis library
+look up the password in redis when someone calls the ./login endpoint
+*/
 
 app.use(bodyParser.json()); // Use body parser
 
@@ -11,9 +22,12 @@ app.listen(port, ()=>{
 }) // listen
 
 app.post('/login',(request, response) =>{
-
+    const requestHashedPassword = md5(request.body.password)
+    const redisHashedPassword=redisClient.hget('passwords', request.body.userName)
     const loginRequest = request.body;
     console.log("Request Body", JSON.stringify(request.body));
+
+
     if (loginRequest.userName == "vredenburgben@gmail.com" && loginRequest.password =="N7T5PpQz!") {
         response.status(200);
         response.send('Welcome');
@@ -26,4 +40,5 @@ app.post('/login',(request, response) =>{
 app.get('/',(request,response)=>{
     response.send('Hello, welcome to localhost')
 }); // response
+
 
