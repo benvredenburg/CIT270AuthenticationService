d3ab105dd97b2fff220ea5e2da423acc897149a7
benvredenburg <vre19001@byui.edu>
2022-05-16 13:47:06 -0600

    05/16/2022

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
 const express = require('express'); // import express library
-const redis = require('redis');
-const redisClient = redis.createClient();
 const bodyParser = require('body-parser'); // import body parser
 const md5 = require('md5'); // import md5
-const port = 3000; // define port const variable
+const {createClient} = require('redis');
+createClient.connect;
 const app = express(); // use express library
-
 app.use(bodyParser.json()); // Use body parser
+const port = 3000; // define port const variable
 
-app.listen(port, ()=>{console.log('listening on port: ' + port);}) // listen
+app.listen(port, ()=>{
+    console.log('listening on port: ' + port);
+}) // listen
 
-app.post('/login',(request, response) =>{
-    const requestHashedPassword = md5(request.body.password)
-    const redisHashedPassword=await redisClient.hget('passwords', request.body.userName)
-    const loginRequest = request.body;
-    console.log("Request Body", JSON.stringify(request.body));
+const validatePassword = async(request, response) => {
+    const requestHashedPassword = md5(request.body.password);
+    const redisHashedPassword = await redisClient.hget('passwords', request.body.userName);
 
-    if (loginRequest.userName == "vredenburgben@gmail.com" && loginRequest.password =="N7T5PpQz!") {
+    const password = await redisClient.hget(request.body.userName);
+
+    if (request.body.userName == 'vredenburgben@gmail.com' && requestHashedPassword == redisHashedPassword) {
         response.status(200);
         response.send('Welcome');
     } else {
         response.status(401);
         response.send('Unauthorized');
     }
-});
+}
 
 app.get('/',(request,response)=>{
     response.send('Hello, welcome to localhost')
 }); // response
-
-
